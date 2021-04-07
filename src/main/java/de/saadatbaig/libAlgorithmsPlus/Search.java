1df0adc420b6a657ce672a49613640a5c15b324d
mass1ve-err0r <me@saadat.dev>
2021-04-07 16:10:46 +0200

    Add Search Class + include firstDupe finder function

---
+++ src/main/java/de/saadatbaig/libAlgorithmsPlus/Search.java
@@ -0,0 +1,35 @@
+/*
+ * Project: libAlgorithmsPlus
+ * Author:  Saadat M. Baig
+ * Date:    07.04.21
+ * License: BSD-3-Clause-Attribution
+ */
+package de.saadatbaig.libAlgorithmsPlus;
+
+
+public class Search {
+
+    /**
+     * <pre>
+     *     Complexity: O(n)
+     * </pre>
+     * Find the first duplicate number inside the array if the following constraint is true:<br>
+     *     Values span from 1 <= N where N is the length of the array.
+     * @param inputArray Array to process.
+     * @return First duplicate number or -1 if not found.
+     */
+    public static int firstDuplicateInArrayNumeric(int[] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            if(inputArray[Math.abs(inputArray[i]) -1] < 0) {
+                return Math.abs(inputArray[i]);
+            } else {
+                inputArray[Math.abs(inputArray[i]) -1] = -inputArray[Math.abs(inputArray[i]) -1];
+            }
+        }
+
+        return -1;
+    }
+
+
+    /* End */
+}
