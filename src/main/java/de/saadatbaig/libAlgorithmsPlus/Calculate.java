b51bec5fbce6bc43112a169df997d64c57fc4dea
mass1ve-err0r <me@saadat.dev>
2021-04-07 17:04:12 +0200

    Update Layout + encapsulate the functions into their groups

--- src/main/java/de/saadatbaig/libAlgorithmsPlus/Calculate.java
+++ src/main/java/de/saadatbaig/libAlgorithmsPlus/Calculate.java
@@ -9,30 +9,35 @@ package de.saadatbaig.libAlgorithmsPlus;
 
 public class Calculate {
 
-    /**
-     * <pre>
-     *     Complexity: O(n)
-     * </pre>
-     * Generate a new array where the index in the output is built of the product of the input except the index's own value.
-     * @param inputArray Array to process.
-     * @return New array of products.
-     */
-    public static int[] productOfArrayExceptSelf(int[] inputArray) {
-        int inputArraySize = inputArray.length;
-        int[] outputArray = new int[inputArraySize];
+    public static class Arrays {
 
-        outputArray[0] = 1;
-        for (int i = 1; i < inputArraySize; i++) {
-            outputArray[i] = inputArray[i-1] * outputArray[i-1];
+        /**
+         * <pre>
+         *     Complexity: O(n)
+         * </pre>
+         * Generate a new array where the index in the output is built of the product of the input except the index's own value.
+         * @param inputArray Array to process.
+         * @return New array of products.
+         */
+        public static int[] productOfArrayExceptSelf(int[] inputArray) {
+            int inputArraySize = inputArray.length;
+            int[] outputArray = new int[inputArraySize];
+
+            outputArray[0] = 1;
+            for (int i = 1; i < inputArraySize; i++) {
+                outputArray[i] = inputArray[i-1] * outputArray[i-1];
+            }
+
+            int rightProducts = 1;
+            for (int i = inputArraySize-1; i >= 0; i--) {
+                outputArray[i] = outputArray[i] * rightProducts;
+                rightProducts *= inputArray[i];
+            }
+
+            return outputArray;
         }
 
-        int rightProducts = 1;
-        for (int i = inputArraySize-1; i >= 0; i--) {
-            outputArray[i] = outputArray[i] * rightProducts;
-            rightProducts *= inputArray[i];
-        }
-
-        return outputArray;
+        /* End */
     }
 
 
--- src/main/java/de/saadatbaig/libAlgorithmsPlus/Search.java
+++ src/main/java/de/saadatbaig/libAlgorithmsPlus/Search.java
@@ -9,25 +9,30 @@ package de.saadatbaig.libAlgorithmsPlus;
 
 public class Search {
 
-    /**
-     * <pre>
-     *     Complexity: O(n)
-     * </pre>
-     * Find the first duplicate number inside the array if the following constraint is true:<br>
-     *     Values span from 1 <= N where N is the length of the array.
-     * @param inputArray Array to process.
-     * @return First duplicate number or -1 if not found.
-     */
-    public static int firstDuplicateInArrayNumeric(int[] inputArray) {
-        for (int i = 0; i < inputArray.length; i++) {
-            if(inputArray[Math.abs(inputArray[i]) -1] < 0) {
-                return Math.abs(inputArray[i]);
-            } else {
-                inputArray[Math.abs(inputArray[i]) -1] = -inputArray[Math.abs(inputArray[i]) -1];
+    public static class Arrays {
+
+        /**
+         * <pre>
+         *     Complexity: O(n)
+         * </pre>
+         * Find the first duplicate number inside the array if the following constraint is true:<br>
+         *     Values span from 1 <= N where N is the length of the array.
+         * @param inputArray Array to process.
+         * @return First duplicate number or -1 if not found.
+         */
+        public static int firstDuplicateInArrayNumeric(int[] inputArray) {
+            for (int i = 0; i < inputArray.length; i++) {
+                if(inputArray[Math.abs(inputArray[i]) -1] < 0) {
+                    return Math.abs(inputArray[i]);
+                } else {
+                    inputArray[Math.abs(inputArray[i]) -1] = -inputArray[Math.abs(inputArray[i]) -1];
+                }
             }
+
+            return -1;
         }
 
-        return -1;
+        /* End */
     }
 
 
