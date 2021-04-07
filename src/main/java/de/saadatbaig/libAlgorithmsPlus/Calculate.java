c0a69ac15b906dfd9cc9bd179037f517d80d411d
mass1ve-err0r <me@saadat.dev>
2021-04-07 16:38:50 +0200

    Add Calculate Class + include productOfArrayExceptSelf generation function

---
+++ src/main/java/de/saadatbaig/libAlgorithmsPlus/Calculate.java
@@ -0,0 +1,40 @@
+/*
+ * Project: libAlgorithmsPlus
+ * Author:  Saadat M. Baig
+ * Date:    07.04.21
+ * License: BSD-3-Clause-Attribution
+ */
+package de.saadatbaig.libAlgorithmsPlus;
+
+
+public class Calculate {
+
+    /**
+     * <pre>
+     *     Complexity: O(n)
+     * </pre>
+     * Generate a new array where the index in the output is built of the product of the input except the index's own value.
+     * @param inputArray Array to process.
+     * @return New array of products.
+     */
+    public static int[] productOfArrayExceptSelf(int[] inputArray) {
+        int inputArraySize = inputArray.length;
+        int[] outputArray = new int[inputArraySize];
+
+        outputArray[0] = 1;
+        for (int i = 1; i < inputArraySize; i++) {
+            outputArray[i] = inputArray[i-1] * outputArray[i-1];
+        }
+
+        int rightProducts = 1;
+        for (int i = inputArraySize-1; i >= 0; i--) {
+            outputArray[i] = outputArray[i] * rightProducts;
+            rightProducts *= inputArray[i];
+        }
+
+        return outputArray;
+    }
+
+
+    /* End */
+}
